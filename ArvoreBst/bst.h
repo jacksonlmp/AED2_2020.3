40865255a5d9de75193bd50757e786c9860fec53
Jackson Lima <jacksonlmp17.jl@gmail.com>
2020-08-27 23:33:50 -0300

    Comentarios

--- ArvoreBst/bst.h
+++ ArvoreBst/bst.h
@@ -7,8 +7,8 @@ typedef struct no{
     struct no *dir;
 }no;
 
-no* inserir(no *raiz, int chave); // ok
-no* remover(no *raiz, int chave); // ok
+no* inserir(no *raiz, int chave); 
+no* remover(no *raiz, int chave); 
 
 no* maior(no *raiz);
 no* menor(no *raiz);
@@ -16,13 +16,13 @@ no* menor(no *raiz);
 no* predecessor(no *raiz, int chave, no* novo);
 no* sucessor(no *raiz, int chave, no* novo);
 
-int quantidade_elementos(no *raiz); // ok
+int quantidade_elementos(no *raiz); 
 int existe(no *raiz, int chave);    //1 ou 0
-int altura(no *raiz);               // ok
+int altura(no *raiz);               
 int MAX(int valorA, int valorB);
 
-void preorder(no *raiz);    //ok
-void inorder(no *raiz);     // ok
-void posorder(no *raiz);    // ok
+void preorder(no *raiz);    
+void inorder(no *raiz);     
+void posorder(no *raiz);    
 
 #endif
